a5419df0a332fe06e3ad4db9166e0c292b04e488
Mayur Joshi <mayurjoshi7@gmail.com>
2016-01-26 13:48:25 +0530

    - Added pch, Macros and Notification-Alert common header files

---
+++ flipper/flipper/flipperMacros.h
@@ -0,0 +1,21 @@
+//
+//  flipperMacros.h
+//  flipper
+//
+//  Created by Mayur Joshi on 26/01/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#ifndef flipperMacros_h
+#define flipperMacros_h
+
+// used for converting web hex color values into UIColor e.g.
+// UIColor *headerColor = UIColorFromRGB(0xff0000);
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+#define APP_DELEGATE ((AppDelegate *)[UIApplication sharedApplication].delegate)
+#define USER_DEFAULTS [NSUserDefaults standardUserDefaults]
+#define MAIN_STORYBOARD [UIStoryboard storyboardWithName:@"Main" bundle:nil]
+#define APP_NAME [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"]
+
+#endif /* flipperMacros_h */
---
+++ flipper/flipper/flipperNotificationsAndAlerts.h
@@ -0,0 +1,13 @@
+//
+//  flipperNotificationsAndAlerts.h
+//  flipper
+//
+//  Created by Mayur Joshi on 26/01/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#ifndef flipperNotificationsAndAlerts_h
+#define flipperNotificationsAndAlerts_h
+
+
+#endif /* flipperNotificationsAndAlerts_h */
