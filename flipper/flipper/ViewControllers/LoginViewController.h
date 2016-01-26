1e044b69d19b40c277f416028ae3ec48e1d1aa2e
Mayur Joshi <mayurjoshi7@gmail.com>
2016-01-26 13:10:09 +0530

    Added Initial Login Controller

---
+++ flipper/flipper/ViewControllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  flipper
+//
+//  Created by Mayur Joshi on 26/01/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ flipper/flipper/ViewControllers/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  flipper
+//
+//  Created by Mayur Joshi on 26/01/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
