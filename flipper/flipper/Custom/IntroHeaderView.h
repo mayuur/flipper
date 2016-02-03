44ed33e4015b8eefb3f469f3019e7c6cb56ba161
Mayur Joshi <mayurjoshi7@gmail.com>
2016-02-03 20:22:43 +0530

    Sign Up & Login Screen UI

---
+++ flipper/flipper/Custom/IntroHeaderView.h
@@ -0,0 +1,15 @@
+//
+//  IntroHeaderView.h
+//  flipper
+//
+//  Created by Ashutosh Dave on 03/02/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntroHeaderView : UIView
+
+@property (weak, nonatomic) IBOutlet UILabel *labelHeaderTitle;
+
+@end
---
+++ flipper/flipper/Custom/IntroTextView.h
@@ -0,0 +1,16 @@
+//
+//  IntroTextView.h
+//  flipper
+//
+//  Created by Ashutosh Dave on 03/02/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntroTextView : UIView
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+@end
