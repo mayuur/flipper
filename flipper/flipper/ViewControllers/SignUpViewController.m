39c19541e82167e26f559b19752940a641e2c77c
Mayur Joshi <mayurjoshi7@gmail.com>
2016-02-03 23:41:17 +0530

    Scroll when keyboard appears in SignUp

--- flipper/flipper/ViewControllers/SignUpViewController.m
+++ flipper/flipper/ViewControllers/SignUpViewController.m
@@ -10,6 +10,8 @@
 #import "IntroHeaderView.h"
 #import "IntroTextView.h"
 
+#define kOFFSET_FOR_KEYBOARD 180.0
+
 @interface SignUpViewController ()
 
 @property (weak, nonatomic) IBOutlet IntroHeaderView *signUpHeaderView;
@@ -17,6 +19,10 @@
 @property (weak, nonatomic) IBOutlet IntroTextView *textViewName;
 @property (weak, nonatomic) IBOutlet IntroTextView *textViewEmail;
 @property (weak, nonatomic) IBOutlet IntroTextView *textViewPassword;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollViewSignUp;
+
+@property (weak, nonatomic) IBOutlet UIView *viewSignUp;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewSignUpBottomConstraint;
 @end
 
 @implementation SignUpViewController
@@ -33,8 +39,84 @@
     [_textViewEmail.imageView setImage:[UIImage imageNamed:@"iconEmail"]];
     
     [_textViewPassword.textField setPlaceholder:@"Password"];
+    [_textViewPassword.textField setSecureTextEntry:YES];
     [_textViewPassword.imageView setImage:[UIImage imageNamed:@"iconPassword"]];
     
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    // register for keyboard notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    // unregister for keyboard notifications while not visible.
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+
+-(void)keyboardWillShow {
+    // Animate the current view out of the way
+    if (_viewSignUpBottomConstraint.constant < kOFFSET_FOR_KEYBOARD)
+    {
+        [self moveScrollViewUp:YES];
+    }
+}
+
+-(void)keyboardWillHide {
+    if (_viewSignUpBottomConstraint.constant >= kOFFSET_FOR_KEYBOARD)
+    {
+        [self moveScrollViewUp:NO];
+    }
+//    else if (_viewSignUpBottomConstraint.constant < kOFFSET_FOR_KEYBOARD)
+//    {
+//        [self moveScrollViewUp:YES];
+//    }
+}
+
+-(void)moveScrollViewUp:(BOOL)movedUp {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3]; // if you want to slide up the view
+    
+    CGRect rect = _viewSignUp.frame;
+    if (movedUp)
+    {
+        // 1. move the view's origin up so that the text field that will be hidden come above the keyboard
+        // 2. increase the size of the view so that the area behind the keyboard is covered up.
+        _viewSignUpBottomConstraint.constant += kOFFSET_FOR_KEYBOARD;
+        [_scrollViewSignUp setContentOffset:CGPointMake(0, kOFFSET_FOR_KEYBOARD) animated:YES];
+//        rect.origin.y -= kOFFSET_FOR_KEYBOARD;
+//        rect.size.height += kOFFSET_FOR_KEYBOARD;
+    }
+    else
+    {
+        // revert back to the normal state.
+        _viewSignUpBottomConstraint.constant -= kOFFSET_FOR_KEYBOARD;
+//        rect.origin.y += kOFFSET_FOR_KEYBOARD;
+//        rect.size.height -= kOFFSET_FOR_KEYBOARD;
+    }
+    _viewSignUp.frame = rect;
+    NSLog(@"Frame:%@",_viewSignUp);
+    [UIView commitAnimations];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -51,5 +133,6 @@
     // Pass the selected object to the new view controller.
 }
 */
+
 
 @end
