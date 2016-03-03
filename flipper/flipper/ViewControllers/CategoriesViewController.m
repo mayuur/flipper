67b8d55198e17955428d133042e04987ead0b526
Mayur Joshi <mayurjoshi7@gmail.com>
2016-03-03 22:57:01 +0530

    - Fetch categories data from Parse

--- flipper/flipper/ViewControllers/CategoriesViewController.m
+++ flipper/flipper/ViewControllers/CategoriesViewController.m
@@ -8,9 +8,12 @@
 
 #import "CategoriesViewController.h"
 #import "CategoriesCollectionViewCell.h"
+#import "Categories.h"
 
 @interface CategoriesViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
-
+{
+    NSMutableArray *arrCategories;
+}
 @property (weak, nonatomic) IBOutlet UICollectionView *categoriesCollectionView;
 
 @end
@@ -20,7 +23,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    arrCategories = [NSMutableArray array];
+    
     [_categoriesCollectionView registerClass:[CategoriesCollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
+    
+    [self getAllCategories];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,13 +42,27 @@
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 10;
+    return arrCategories.count;
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     CategoriesCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
     cell.backgroundColor = [UIColor redColor];
     return cell;
+}
+
+#pragma mark - Custom Methods
+
+-(void)getAllCategories {
+    PFQuery *query = [PFQuery queryWithClassName:[Categories parseClassName]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        if(!error){
+            [arrCategories addObjectsFromArray:objects];
+            [self.categoriesCollectionView reloadData];
+        }else {
+            NSLog(@"Error:%@",error.localizedDescription);
+        }
+    }];
 }
 
 /*
