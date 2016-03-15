8347a53eb49e9cb97a59ca17b85563b3c88e0065
Mayur Joshi <mayurjoshi7@gmail.com>
2016-03-16 01:30:35 +0530

    - Fetch people data from Parse

--- flipper/flipper/ViewControllers/FollowPeopleViewController.m
+++ flipper/flipper/ViewControllers/FollowPeopleViewController.m
@@ -7,17 +7,79 @@
 //
 
 #import "FollowPeopleViewController.h"
+#import "PeopleTableViewCell.h"
+#import "IntroHeaderView.h"
+#import "People.h"
+
+@interface FollowPeopleViewController ()<UITableViewDataSource,UITableViewDelegate>
+{
+    
+    __weak IBOutlet IntroHeaderView *peopleHeader;
+    __weak IBOutlet UITableView *tablePeople;
+    NSMutableArray *arrayPeople,*arrImages;
+}
+
+@end
 
 @implementation FollowPeopleViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [peopleHeader.labelHeaderTitle setText:@"Follow"];
+    
+    arrayPeople = [NSMutableArray array];
+    arrImages = [NSMutableArray array];
+    [self getPeopleDataFrom:@[@"ua70boG3jc",@"OTNCmSBXeL"]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView DataSource
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return arrayPeople.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    PeopleTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PeopleCell"];
+    
+    People *tempPeople = [arrayPeople objectAtIndex:indexPath.row];
+    [cell.labelName setText:tempPeople.person_name];
+    [cell.imagePerson setImage:[UIImage imageWithData:[arrImages objectAtIndex:indexPath.row]]];
+    
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+
+#pragma mark - Custom Methods
+
+-(void)getPeopleDataFrom:(NSArray *)array {
+    for (NSString *temp in array) {
+        PFQuery *query = [PFQuery queryWithClassName:[People parseClassName]];
+        [query whereKey:@"fk_category_id" equalTo:temp];
+        [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+            if(!error){
+                [arrayPeople addObjectsFromArray:objects];
+                
+                for (People *tempPeople in objects) {
+                    [tempPeople.person_image getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+                        [arrImages addObject:data];
+                        if (arrImages.count == arrayPeople.count) {
+                            [tablePeople reloadData];
+                        }
+                    }];
+                }
+            }else {
+                NSLog(@"Error:%@",error.localizedDescription);
+            }
+        }];
+
+    }
 }
 
 #pragma mark - Button actions
