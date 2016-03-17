57fde0d7e3f814a8d34be1b6685bc1439ef6fce3
Mayur Joshi <mayurjoshi7@gmail.com>
2016-03-17 07:25:50 +0530

    - Added category for App Colors

---
+++ flipper/flipper/Categories/UIColor+AppColors.h
@@ -0,0 +1,14 @@
+//
+//  UIColor+AppColors.h
+//  flipper
+//
+//  Created by Mayur Joshi on 17/03/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (AppColors)
+
++ (UIColor* ) followPeopleBlueBackground;
+@end
---
+++ flipper/flipper/Categories/UIColor+AppColors.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+AppColors.m
+//  flipper
+//
+//  Created by Mayur Joshi on 17/03/16.
+//  Copyright © 2016 Mayur Joshi. All rights reserved.
+//
+
+#import "UIColor+AppColors.h"
+
+@implementation UIColor (AppColors)
+
++ (UIColor* ) followPeopleBlueBackground {
+    return UIColorFromRGB(0xff0000);
+}
+
+@end
