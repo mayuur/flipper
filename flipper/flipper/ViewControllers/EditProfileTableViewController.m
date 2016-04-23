97a3db67dc0c8c5b8227cf9c6dafc130eec70c4a
Mayur Joshi <mayurjoshi7@gmail.com>
2016-04-24 03:30:33 +0530

    - Fixed issue with Username/Password being editable textfields in Profile section

--- flipper/flipper/ViewControllers/EditProfileTableViewController.m
+++ flipper/flipper/ViewControllers/EditProfileTableViewController.m
@@ -26,6 +26,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    textFieldEmail.enabled = NO;
+    textFieldPassword.enabled = NO;
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
